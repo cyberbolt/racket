adcaea7872810d85a8c92c16ee18da5303c51e3b
Bulat Zamilov <bulat.zamilov@gmail.com>
2015-08-05 19:32:34 +0300

    Add checkboard and other complex figures

--- intro.rkt
+++ intro.rkt
@@ -1,24 +1,32 @@
 #lang slideshow
+
 (define c (circle 10))
+
 (define r (rectangle 13 20))
+
 (define (square n)
   ;draws filled square
   (filled-rectangle n n))
+
 (define s (square 10))
+
 (define (four p)
   ;draws four figures squared
   (define two-p (hc-append p p))
   (vc-append two-p two-p))
+
 (define (checker p1 p2)
   (let ([p12 (hc-append p1 p2)]
         [p21 (hc-append p2 p1)])
     (vc-append p12 p21)))
+
 (define (checkerboard p)
   (let* ([rp (colorize p "red")]
          [bp (colorize p "black")]
          [c (checker rp bp)]
          [c4 (four c)])
     (four c4)))
+
 (define (series mk)
   ;functions are values
   (hc-append 4 (mk 5) (mk 10) (mk 20)))
